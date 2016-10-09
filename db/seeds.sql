245b70f65987f2562cf13d80808ee93984dcc781
illynabruin92 <alvaivette@g.ucla.edu>
2016-10-08 21:03:55 -0700

    schema.sql and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+//write insert queries to populate burgers table w/ 3+ entries
+
+INSERT INTO burgers(burger_name, devoured) VALUES ('Poblano Picasso Burger', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Eggers Can\'t Be Cheesers (with fried egg & cheese)', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Beets of Burden Burger (served with too many beets)', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Last of the Mo-jicama (comes with jicama)', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Cheeses Is Born Burger (comes with baby Swiss cheese)', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Don\'t You Four Cheddar \'Bout Me Burger (comes with four kinds of cheddar)', false);
