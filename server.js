a7b8debba06cc90e11d19d177db3feecd3f14f50
illynabruin92 <alvaivette@g.ucla.edu>
2016-10-08 18:18:06 -0700

    update require/port

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 
 // Initialize Express app
 var express = require('express');
-var app = express();
 
 // include method-override
 var methodOverride = require('method-override');
@@ -11,6 +10,28 @@ var methodOverride = require('method-override');
 //include bodyParser
 var bodyParser = require('body-parser');
 
+//set up express
+var app = express();
+
+//static app content from public directory
+app.use(express.static(process.cwd() + '/public'));
+
 // bodyParser middleware
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false })); 
+
+//POST method override
+app.use(methodOverride('_method'));
+
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//routes
+var routes = require('./addyourburger/burger_add.js');
+app.use('/', routes);
+
+var port = 3000;
+app.listen(port);
